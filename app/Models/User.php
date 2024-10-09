0bf6ad497d806d67ebbafaf1a89c867cc52c267a
Hiba Haseeb <hibafreelancing@gmail.com>
2024-10-09 09:58:31 +0500

    User Model updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,15 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;  // Import SoftDeletes
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;  // Add SoftDeletes trait
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
+    use SoftDeletes;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
