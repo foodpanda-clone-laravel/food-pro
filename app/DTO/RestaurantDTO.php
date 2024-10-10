cc5013e075f4b080b53f0adab1ba3334a7bd406a
Junaid Khan <junaidsajidkhan@gmail.com>
2024-10-10 16:43:23 +0500

    Feat:Restuarant/Owner Register

--- app/DTO/RestaurantDTO.php
+++ app/DTO/RestaurantDTO.php
@@ -6,11 +6,6 @@ class RestaurantDTO extends BaseDTO
 {
     public string $name;
     public int $owner_id;
-    public int $branch_id;
-    public ?int $branches;
-    public string $address;
-    public string $postal_code;
-    public string $city;
     public ?string $opening_time;
     public ?string $closing_time;
     public string $cuisine;
@@ -20,11 +15,6 @@ class RestaurantDTO extends BaseDTO
     public function __construct(
         string $name,
         int $owner_id,
-        int $branch_id,
-        ?int $branches = null,
-        string $address,
-        string $postal_code,
-        string $city,
         ?string $opening_time = null,
         ?string $closing_time = null,
         string $cuisine,
@@ -33,11 +23,6 @@ class RestaurantDTO extends BaseDTO
     ) {
         $this->name = $name;
         $this->owner_id = $owner_id;
-        $this->branch_id = $branch_id;
-        $this->branches = $branches;
-        $this->address = $address;
-        $this->postal_code = $postal_code;
-        $this->city = $city;
         $this->opening_time = $opening_time;
         $this->closing_time = $closing_time;
         $this->cuisine = $cuisine;
