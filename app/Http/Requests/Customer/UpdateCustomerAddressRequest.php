f3f946fb2a1ef45390a624a1f7ab9e5cd93fba45
Hiba Haseeb <hibafreelancing@gmail.com>
2024-10-10 16:38:48 +0500

    Merge Customer Code

--- app/Http/Requests/Customer/UpdateCustomerAddressRequest.php
+++ app/Http/Requests/Customer/UpdateCustomerAddressRequest.php
@@ -3,29 +3,16 @@
 namespace App\Http\Requests\Customer;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\BaseRequest;
 
 class UpdateCustomerAddressRequest extends BaseRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, mixed>
-     */
     public function rules()
     {
         return [
             'address' => 'sometimes|string|min:5',
-            'delivery_address' => 'sometimes|nullable|string|min:5',
+            'delivery_address' => 'sometimes|string|min:5',
         ];
     }
 }
