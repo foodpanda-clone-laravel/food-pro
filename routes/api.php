2277cf38fbfef3d600819de5238280c0ceb6940b
hadiya8149 <hadiya8149@gmail.com>
2024-10-11 12:46:14 +0500

    resolve conflict with main branch

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Orders\CartController;
+use App\Http\Controllers\RestaurantOwner\MenuController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Database\QueryException;
 use App\Http\Controllers\Customer\CustomerController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
-use App\Http\Controllers\RestaurantOwner\RestaurantController;
 use App\Http\Controllers\Auth\UserController;
 
 
@@ -20,8 +22,8 @@ use App\Http\Controllers\Auth\UserController;
 |
 */
 
-Route::post('register', [UserController::class, 'register']);
-Route::post('/restaurant/register-with-owner', [RestaurantController::class, 'registerRestaurantWithOwner']);
+Route::post('/register', [RegisterController::class, 'signup']);
+Route::post('/register-business', [RegisterController::class, 'registerRestaurantWithOwner']);
 Route::post('/login', [UserController::class, 'login']);
 
 Route::middleware(['request.logs', 'jwt'])->group(function () {
@@ -49,17 +51,19 @@ Route::middleware(['request.logs', 'jwt'])->group(function () {
         return response()->json($request->auth);
 
     });
-     Route::post('create-menu/{branch_id}',[MenuController::class, 'createMenu']);
-        Route::post('add-item/menu/{menu_id}', [MenuController::class, 'addMenuItem']);
-        Route::post('add-addon/menu/{menu_item_id}', [MenuController::class, 'addOns']);
-        Route::post('update-menu/{menu_item}', [MenuController::class, 'updateMenu']);
-        Route::post('update-menu-item/{menu_item_id}', [MenuController::class, 'updateMenuItem']);
-        Route::post('add-choice/{menu_id}', [MenuController::class, 'storeChoices']);
+    Route::post('create-menu/{branch_id}', [MenuController::class, 'createMenu']);
+    Route::post('add-item/menu/{menu_id}', [MenuController::class, 'addMenuItem']);
+    Route::post('add-addon/menu/{menu_item_id}', [MenuController::class, 'addOns']);
+    Route::post('update-menu/{menu_item}', [MenuController::class, 'updateMenu']);
+    Route::post('update-menu-item/{menu_item_id}', [MenuController::class, 'updateMenuItem']);
+    Route::post('add-choice/{menu_id}', [MenuController::class, 'storeChoices']);
 
     Route::controller(ForgotPasswordController::class)->group(function () {
         Route::post('/forgot-password', 'submitForgotPasswordForm')->name('password.email');
-        
+
         Route::post('/reset-password', 'submitResetPasswordForm')->name('password.update');
     });
-
+    Route::controller(CartController::class)->group(function(){
+        Route::get('/session', 'getShoppingSession');
+    });
 });
