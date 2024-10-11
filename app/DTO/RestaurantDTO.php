cd977af2714658c4388b330bef90fc17ef3b9ad5
aliishoaib <102282470+aliishoaib@users.noreply.github.com>
2024-10-11 11:16:32 +0500

    Update RestaurantDTO.php

--- app/DTO/RestaurantDTO.php
+++ app/DTO/RestaurantDTO.php
@@ -6,29 +6,19 @@ class RestaurantDTO extends BaseDTO
 {
     public string $name;
     public int $owner_id;
-    public int $branch_id;
-    public ?int $branches;
-    public string $address;
-    public string $postal_code;
-    public string $city;
     public ?string $opening_time;
     public ?string $closing_time;
     public string $cuisine;
     public ?string $logo_path;
     public string $business_type;
 
-    public function __construct(array $data) {
-        $this->name = $data['name'];
-        $this->owner_id = $data['owner_id'];
-        $this->branch_id = $data['branch_id'];
-        $this->branches = $data['branches'] ?? null; // Nullable field
-        $this->address = $data['address'];
-        $this->postal_code = $data['postal_code'];
-        $this->city = $data['city'];
-        $this->opening_time = $data['opening_time'] ?? null; // Nullable field
-        $this->closing_time = $data['closing_time'] ?? null; // Nullable field
-        $this->cuisine = $data['cuisine'];
-        $this->logo_path = $data['logo_path'] ?? null; // Nullable field
-        $this->business_type = $data['business_type'];
+    public function __construct($data) {
+        $this->name = $data["restaurant_name"];
+        $this->owner_id = $data["owner_id"];
+        $this->opening_time = $data["opening_time"].':00';
+        $this->closing_time = $data["closing_time"].':00';
+        $this->cuisine = $data["cuisine"];
+        $this->logo_path = $data["logo_path"];
+        $this->business_type = $data["business_type"];
     }
 }
