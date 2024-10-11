7c78635772c7cb313541975ca2a749e1e263d0eb
hadiya8149 <hadiya8149@gmail.com>
2024-10-11 20:12:08 +0500

    Update CustomerDTO.php

--- app/DTO/CustomerDTO.php
+++ app/DTO/CustomerDTO.php
@@ -1,47 +1,19 @@
 <?php
 
-
-
 namespace App\DTO;
 
-
-
 class CustomerDTO extends BaseDTO
-
 {
-
-    public int $user_id;
-
+    public ?int $user_id;
     public ?string $address;
-
     public ?string $delivery_address;
+    public $favorites; // This can be array or string
 
-    public $favorites;
-
-   
-
-
-
-    public function __construct(
-
-       
-
-         array $data
-
-     
-
-    ) {
-
-        $this->user_id=$data['user_id'];
-
+    public function __construct(array $data = [])
+    {
+        $this->user_id = $data['user_id'] ?? null;
         $this->address = $data['address'] ?? null;
-
         $this->delivery_address = $data['delivery_address'] ?? null;
-
-        $this->favorites =  $data['favorites'] ?? null;
-
-       
-
+        $this->favorites = $data['favorites'] ?? null;
     }
-
 }
