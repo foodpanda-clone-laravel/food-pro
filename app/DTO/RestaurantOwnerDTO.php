0d3482d40f7b69500724227e1c34982ae80792cd
aliishoaib <102282470+aliishoaib@users.noreply.github.com>
2024-10-11 11:18:41 +0500

    Update RestaurantOwnerDTO.php

--- app/DTO/RestaurantOwnerDTO.php
+++ app/DTO/RestaurantOwnerDTO.php
@@ -10,11 +10,13 @@ class RestaurantOwnerDTO extends BaseDTO
     public string $iban;
     public string $account_owner_title;
 
+
     public function __construct($data) {
-        $this->cnic = $data['cnic'];
-        $this->user_id = $data['user_id'];
-        $this->bank_name = $data['bank_name'];
-        $this->iban = $data['iban'];
-        $this->account_owner_title = $data['account_owner_title'];
+        $this->cnic = $data["cnic"];
+        $this->user_id = $data["user_id"];
+        $this->bank_name = $data["bank_name"];
+        $this->iban = $data["iban"];
+        $this->account_owner_title = $data["account_owner_title"];
+
     }
 }
