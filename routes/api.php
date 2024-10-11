5e9a7abfd55983fd3de148cbfb1918c6c7629b0d
hadiya8149 <hadiya8149@gmail.com>
2024-10-11 20:16:46 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,6 @@ use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\UserController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\AuthController;
-
 use App\Http\Controllers\Menu\MenuController;
 
 /*
