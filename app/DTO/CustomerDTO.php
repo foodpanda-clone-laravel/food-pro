0069712444adaa98d46f656ad1b13d1208001480
aliishoaib <102282470+aliishoaib@users.noreply.github.com>
2024-10-11 11:12:34 +0500

    Update CustomerDTO.php

--- app/DTO/CustomerDTO.php
+++ app/DTO/CustomerDTO.php
@@ -5,14 +5,14 @@ namespace App\DTO;
 class CustomerDTO extends BaseDTO
 {
     public int $user_id;
-    public string $address;
-    public ?string $delivery_address; // Nullable
-    public $favorites; // This can be an array or string depending on your choice
+    public ?string $address;
+    public ?string $delivery_address;
+    public $favorites; 
 
     public function __construct(array $data) {
         $this->user_id = $data['user_id'];
         $this->address = $data['address'];
-        $this->delivery_address = $data['delivery_address'] ?? null; // Default to null if not provided
-        $this->favorites = $data['favorites']; // Can be array or string based on input
+        $this->delivery_address = $data['delivery_address'] ?? null;
+        $this->favorites = $data['favorites']; 
     }
 }
