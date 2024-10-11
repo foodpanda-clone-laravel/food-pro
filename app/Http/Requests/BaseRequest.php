25c82a473bd5653b21e1856b12fb0aeb92e24f74
aliishoaib <102282470+aliishoaib@users.noreply.github.com>
2024-10-11 11:20:34 +0500

    Update BaseRequest.php

--- app/Http/Requests/BaseRequest.php
+++ app/Http/Requests/BaseRequest.php
@@ -8,21 +8,18 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Contracts\Validation\Validator;
 class BaseRequest extends FormRequest
 {
-    
-// override failed validation once in base class
+
+    // override failed validation once in base class
     protected function failedValidation(Validator $validator)
     {
         $errors = $this->validator->errors();
 
-        $response =  response()->json([
-            'errors'=>$errors
+        $response = response()->json([
+            'errors' => $errors
         ], 400);
         throw new HttpResponseException($response);
     }
-
     public function getValidatedData(){
-
         return $this->validated();
-
     }
 }
