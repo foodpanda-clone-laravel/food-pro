de054a8d5b48b347e9d840b8c8c187e3adc3f914
hadiya8149 <hadiya8149@gmail.com>
2024-10-14 17:09:53 +0000

    resolve conflict with api.php main branch

--- routes/api.php
+++ routes/api.php
@@ -22,41 +22,16 @@ use App\Http\Controllers\Customer\OrderController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+require __DIR__ . '/restaurant/restaurantapi.php';
+
+require __DIR__ . '/order/orderapi.php';
+require __DIR__ . '/Customer/customerapi.php';
+require __DIR__ . '/hibacustomerRoutes/customer.php';
 
 Route::post('/register', [RegisterController::class, 'signup']);
 Route::post('/register-business', [RegisterController::class, 'registerRestaurantWithOwner']);
 Route::post('/login', [UserController::class, 'login']);
 
-Route::middleware(['request.logs', 'jwt'])->group(function () {
-
-    // Customer-related routes
-    Route::prefix('customers')->group(function () {
-        Route::controller(CustomerController::class)->group(function () {
-            Route::patch('edit-profile', 'editProfile');
-            Route::get('favorites', 'favoriteItems');
-            Route::get('rewards', 'viewRewards');
-            Route::post('use-points', 'usePointsAtCheckout');
-            Route::patch('update-address', 'updateCustomerAddress')->name('updateCustomerAddress');
-            Route::get('profile', 'viewProfile');
-            Route::post('favorite-restaurants', 'addFavoriteRestaurant');
-            Route::delete('favorite-restaurants', 'removeFavoriteRestaurant');
-            Route::post('feedback', 'submitFeedback');
-            Route::get('menus', 'viewMenus');
-            Route::get('search-restaurant', 'searchRestaurant');
-            Route::get('restaurants', 'viewAllRestaurants');
-            Route::get('deals', 'viewDeals');
-        });
-    });
-
-    // Order-related routes
-    Route::prefix('orders')->group(function () {
-        Route::controller(OrderController::class)->group(function () {
-            Route::get('history', 'orderHistory');
-            Route::get('active-order', 'activeOrder');
-        });
-
-    });
-});
 
 // Test user route (authenticated)
 Route::get('/user', function (Request $request) {
