53cd7bb727aef046635d840ffe9b310ad348cf3f
hadiya8149 <hadiya8149@gmail.com>
2024-10-17 17:37:24 +0500

    update admin.php

--- routes/Admin/admin.php
+++ routes/Admin/admin.php
@@ -11,5 +11,4 @@ use Illuminate\Support\Facades\Route;
 Route::controller(AdminController::class)->group(function () {
     Route::get('get-applications', 'viewRestaurantApplications');
     Route::post('accept-application/{request_id}', 'approveRequest');
-    
- });  
+ });
